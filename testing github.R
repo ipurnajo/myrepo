e37436341feaba977bd897e2c60c3145a4c3a309
ipurnajo <ipurnajo@gmail.com>
2019-08-08 15:50:15 -0700

    add demographics data

--- testing github.R
+++ testing github.R
@@ -1,1 +1,11 @@
 # Add first line
+
+library(tidyverse)
+library(data.table)
+library(broom)
+library(magrittr)
+library(table1)
+
+demographics <- fread("R:/Clinical Trials/BRISTOL-MYERS SQUIBB/Graft Function/Data/BMS-BasicDemographics_DATA_2019-07-11_1727.csv")
+
+head(demographics)
